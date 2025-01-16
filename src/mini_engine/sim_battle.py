e577c15495fd0ad71e1c85082ad34a60ab4b6b34
albertezem <ezemosakwea@carleton.edu>
2025-01-16 14:06:03 -0600

    starting mini engine

--- src/mini_engine/sim_battle.py
+++ src/mini_engine/sim_battle.py
@@ -1,1 +1,13 @@
 import poke_battle_sim as pb
+
+class BattleSim:
+    def __init__(self, battle: pb.Battle) -> None:
+        self.battle = battle
+        
+    def get_battle_info(self):
+        # Get both pokemon info
+        # For each pokemon, get their stats, cur hp, and moves
+        pass
+    
+    def simulate_turn(self, move1, move2):
+        pass
