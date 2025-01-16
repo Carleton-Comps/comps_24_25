09aee07dc27581d2770fa93d26f6bfb7eed16b32
albertezem <ezemosakwea@carleton.edu>
2025-01-16 14:29:26 -0600

    initialized pokemon

--- src/mini_engine/sim_battle.py
+++ src/mini_engine/sim_battle.py
@@ -3,11 +3,15 @@ import poke_battle_sim as pb
 class BattleSim:
     def __init__(self, battle: pb.Battle) -> None:
         self.battle = battle
-        
+        self.pokemon_1 = {"name": "", "stats": [], "cur_hp": 0, "type": set(), "fainted": False}
+        self.pokemon_2 = {"name": "", "stats": [], "cur_hp": 0, "type": set(), "fainted": False}
+
     def get_battle_info(self):
         # Get both pokemon info
         # For each pokemon, get their stats, cur hp, and moves
+
         pass
     
+    def damage_
     def simulate_turn(self, move1, move2):
         pass
