eb3d16fc96e498a8bc44a8220ef4117c021afa98
tanakas2499 <129445729+tanakas2499@users.noreply.github.com>
2025-01-16 14:30:46 -0600

    w/ albert

--- src/mini_engine/sim_battle.py
+++ src/mini_engine/sim_battle.py
@@ -7,6 +7,23 @@ class BattleSim:
     def get_battle_info(self):
         # Get both pokemon info
         # For each pokemon, get their stats, cur hp, and moves
+
+
+        poke_info = {}
+        
+        # Trainer 1 (Player)
+        pokemon_1 = self.battle.t1.current_poke
+        poke_info["pokemon_1"] = {
+            "name": self.battle.t1.name,
+            "pokemon": {
+                "name": pokemon_1.name,
+                "cur_hp": pokemon_1.cur_hp,
+                "max_hp": pokemon_1.max_hp,
+                "stats": pokemon_1.stats_base,
+                "types": pokemon_1.types,
+            },
+        }
+
         pass
     
     def simulate_turn(self, move1, move2):
